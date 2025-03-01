39b406a687163affdf883469dba10a61c8b3b8bc
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-03-01 22:34:22 +0600

    added new file

---
+++ arraymethods.js
@@ -0,0 +1,7 @@
+const products = [
+    { name: 'laptop', price: '1200 Usd', brand: 'MacBook pro', color: 'gray' },
+    { name: 'laptop', price: '1000 Usd', brand: 'lenovo', color: 'black' },
+    { name: 'laptop', price: '1200 Usd', brand: 'Asus ZenBook', color: 'gray' },
+    { name: 'laptop', price: '1200 Usd', brand: 'Acer', color: 'silver' }
+
+]
