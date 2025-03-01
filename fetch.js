182adf8ededdd809b4951e7e96f6c886801bc976
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-03-02 02:11:45 +0600

    completed code

--- fetch.js
+++ fetch.js
@@ -18,3 +18,43 @@ console.log(studentObj);
 
 // fetch 
 
+// fetch('url')
+//     .then(res => res.json())
+//     .then(data => console.log(data));
+
+// keys and values
+
+const keys = Object.keys(student);
+const values = Object.values(student);
+
+// loop in array
+
+const numbers = [10, 20, 40, 65, 32];
+numbers.forEach(num => console.log(num));
+numbers.map(num => num * 2);
+
+// we used for of on array like objects
+// loop on an object using for in
+
+// add or remove an array
+
+// array of objects
+const products = [
+    { name: 'laptop', price: '1400 Usd', brand: 'MacBook pro', color: 'gray' },
+    { name: 'laptop', price: '1000 Usd', brand: 'lenovo', color: 'black' },
+    { name: 'phone', price: '1100 Usd', brand: 'Iphone', color: 'gray' },
+    { name: 'watch', price: '900 Usd', brand: 'titan', color: 'silver' }
+
+];
+
+const newProduct = { name: 'webcam', price: 1000, brand: 'xiaomi' };
+
+//copy products array and then add newProduct
+// ... means copy something
+const newProducts = [...products, newProduct];
+
+
+// create an new array without one specific item
+// remove phone means creates a new array without the laptop
+
+const remaining = products.filter(product => product.name !== 'laptop');
