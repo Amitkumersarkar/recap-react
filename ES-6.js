1cb4ae82e3ecb792cf4dd01a612d2357db27984a
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-03-01 20:16:45 +0600

    added new updated file

--- ES-6.js
+++ ES-6.js
@@ -1,4 +1,4 @@
-const numbers = [10, 20, 3, 40];
+const numbers = [10, 20, 1986804513, 40];
 const student = {
     name: 'amit sarkar',
     age: 24,
@@ -8,5 +8,5 @@ const student = {
 
 // create an template string
 
-const about = `My name is ${student.name} age of ${student.age} has number ${numbers[2]}`;
+const about = `My name is ${student.name} . and i am ${student.age} years old. here is my personal phone number ${numbers[2]}. i am also an ${student.profession[0]}`;
 console.log(about);
