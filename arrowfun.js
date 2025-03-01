6a903ba76254cf4d928b3d0f67499cc7fa7f1b94
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-03-01 21:54:28 +0600

    added new arrow function file

--- arrowfun.js
+++ arrowfun.js
@@ -2,4 +2,17 @@
 const getFifty = () => 50;
 // single parameterized arrow function declaration
 const addSixty = num => num + 60;
+// checking number is even or not
 
+const isEven = x => x % 2 == 0;
+
+// multiple parameter arrow function
+const addThree = (x, y, z) => x + y + z;
+const result = addThree(10, 20, 15);
+console.log(result);
+
+// multiline parameter
+const doMath = (num1, num2) => {
+    const sum = num1 + num2;
+    return sum;
+}
