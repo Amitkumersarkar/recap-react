007f5cdbc30a5e712d9eecaee99e4b84ee479e60
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-03-02 23:52:15 +0600

    added updated code

--- shortcutCondi.js
+++ shortcutCondi.js
@@ -2,10 +2,20 @@
 // '',0,false,null,undefined
 // truthy falsy shortcut conditions
 
+// here we check truthy value
 let myVal = 7;
+// check any truthy
 if (myVal) {
     myVal = myVal * 50;
 }
 else {
     myVal = 0;
+}
+
+// here we check negative or falsy value
+
+let myAmount = 40;
+// check any falsy value
+if (!myAmount) {
+
 }
