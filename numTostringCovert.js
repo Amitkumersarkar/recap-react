803168df9925d7c9bc7e4317d3d0eda2b1ef73ee
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-03-03 00:24:24 +0600

    added updated file

---
+++ numTostringCovert.js
@@ -0,0 +1,10 @@
+// shortcut number to string conversion
+const num = 100;
+// console.log(num);
+const numStr = num + '';
+// console.log(numStr);
+
+// string to number conversion
+const input = '450';
+const inputNum = +input;
+console.log(inputNum);
