98d7b0a8b13cd27cbc302b965f6ddd953b4afb22
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-03-03 00:32:28 +0600

    added new file

---
+++ shortcutTurtyvalue.js
@@ -0,0 +1,7 @@
+// finding shortcut truthy value
+// const isActive = true;
+const isActive = false;
+const showUser = () => console.log('display User');
+const hideUser = () => console.log('hide User');
+
+isActive ? showUser() : hideUser();
