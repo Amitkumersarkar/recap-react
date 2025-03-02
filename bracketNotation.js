ad6a249166f1eabd8472bc2effb4f837baf3555f
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-03-03 01:44:30 +0600

    added bracket notaion file

---
+++ bracketNotation.js
@@ -0,0 +1,11 @@
+const person = {
+    name: 'Amit Sarkar',
+    profession: 'web developer',
+    age: 24,
+    address: 'Mirzapur'
+}
+// here we used bracket notation
+const prof2 = person["profession"];
+const pName = 'profession';
+const prof = person[pName];
+console.log(prof2);
