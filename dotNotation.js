9bc65854e9ccbea5b5e5db26fda1607765c80582
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-03-03 01:38:40 +0600

    added dot notaion file

---
+++ dotNotation.js
@@ -0,0 +1,11 @@
+const person = {
+    name: 'Amit Sarkar',
+    profession: 'web developer',
+    age: 24,
+    address: 'Mirzapur'
+}
+
+// using dot Notation to access the value
+const prof = person.name;
+// console.log(person);
+
