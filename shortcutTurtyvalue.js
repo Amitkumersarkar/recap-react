7902140ce2ed5fd734c4ea9d9913bea2d9ac140d
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-03-03 00:38:09 +0600

    final update

--- shortcutTurtyvalue.js
+++ shortcutTurtyvalue.js
@@ -5,4 +5,9 @@ const showUser = () => console.log('display User');
 const hideUser = () => console.log('hide User');
 
 isActive ? showUser() : hideUser();
+
+// use  && {and} if the left side is true then right side will be executed
 // isActive && showUser();
+
+// use {or} || if one of the value is true
+// isActive || showUser();
