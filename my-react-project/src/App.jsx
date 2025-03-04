d46c99ddb57d80be70cbb8f60b167a377ca0ea6c
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-03-04 23:12:57 +0600

    update the code

--- my-react-project/src/App.jsx
+++ my-react-project/src/App.jsx
@@ -8,11 +8,21 @@ function App() {
   return (
     <>
       <Person></Person>
+      <Student></Student>
     </>
   )
 }
 function Person() {
+  const person = { name: 'Amit Sarkar' }
   const age = 24;
-  return <h3>i am a person with age : {age}</h3>
+  return <h3>my name is {person.name} and my age is : {age}</h3>
+}
+
+function Student() {
+  return (
+    <div>
+      <h3>i am a student</h3>
+    </div>
+  )
 }
 export default App
