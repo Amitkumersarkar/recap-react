84de108b430a4e520be0e9bc015598965f5af6bc
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-03-06 00:31:12 +0600

    added new event

--- react-practice-file/src/App.jsx
+++ react-practice-file/src/App.jsx
@@ -2,10 +2,15 @@ import './App.css'
 
 function App() {
 
+  function HandleClick() {
+    alert('button click');
+  }
+
   return (
     <>
       <div>
         <h2>react core concept 2.0</h2>
+        <button onClick={HandleClick}>Click Here</button>
       </div>
     </>
   )
