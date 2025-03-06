9b4224454bc866153a3dcee1840f0ea779349dd5
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-03-06 14:49:19 +0600

    added new file

---
+++ react-practice-file/src/User.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react"
+
+export default function Users() {
+    const [users, setUsers] = useState([]);
+
+    // call the useEffect
+    // useEffect has two function one callback another is dependence
+    useEffect(() => {
+
+    }, []);
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+// declared a state to hold the data
+// 
