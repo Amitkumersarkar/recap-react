6f2fd1ea8f22d893545c2c1f4a7b2f9eea950a76
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-03-07 14:14:36 +0600

    added map

--- react-practice-file/src/Friends.jsx
+++ react-practice-file/src/Friends.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import './Friends.css';
+import Friend from './Friend';
 export default function Friends() {
 
     const [friends, setFriends] = useState([]);
@@ -11,6 +12,9 @@ export default function Friends() {
     return (
         <div className='box'>
             <h3>Friends : {friends.length}</h3>
+            {
+                friends.map(friend => <Friend></Friend>)
+            }
         </div>
     )
 }
@@ -20,3 +24,4 @@ export default function Friends() {
 // step2 : declared an useEffect with dependency array to load data
 // step3 : use fetch to load data
 // step4 : loaded data to the state
+// step5 : to show all data we can use loop then use map
