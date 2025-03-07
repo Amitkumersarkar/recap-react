5f2e8d6d95b1249f970fbae7a2dc21bb137171f8
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-03-07 14:26:58 +0600

    final update the code

--- react-practice-file/src/Friend.jsx
+++ react-practice-file/src/Friend.jsx
@@ -2,7 +2,7 @@ export default function Friend({ friend }) {
     // destructure friends objects
     const { name, email } = friend;
     return (
-        <div>
+        <div className="box">
             <h4>Name : {name}</h4>
             <p>Email : {email}</p>
         </div>
