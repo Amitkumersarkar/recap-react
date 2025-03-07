57079ca3a9cc2b6985e8062200b877039c707700
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-03-07 23:05:01 +0600

    updated file

--- practice-react-apps/src/components/Countries/Country/Country.jsx
+++ practice-react-apps/src/components/Countries/Country/Country.jsx
@@ -1,11 +1,13 @@
 import '../Country.css'
 const Country = ({ country }) => {
     // destructuring here
-    const { name, flags } = country;
+    const { name, flags, population, area } = country;
     return (
         <div className="country">
             <h2>Name : {name?.common}</h2>
             <img src={flags.png} alt="" />
+            <p>Population : {population}</p>
+            <p>Area : {area}</p>
         </div>
     );
 };
