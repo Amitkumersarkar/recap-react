a54bfd78cb17791ff96d6796ac63886ae46d9c1f
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-03-07 21:43:15 +0600

    update the code

--- practice-react-apps/src/components/Countries/Countries.jsx
+++ practice-react-apps/src/components/Countries/Countries.jsx
@@ -1,9 +1,18 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 const Countries = () => {
-
-
+    // to store data in useState
+    const [countries, setCountries] = useState([]);
+    // to load data in useEffect
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+            .then(res => res.json())
+            .then(data => setCountries(data))
+    }, [])
     return (
         <div>
-            <h3>Countries: </h3>
+            <h3>Countries : {countries}</h3>
         </div>
     );
 };
