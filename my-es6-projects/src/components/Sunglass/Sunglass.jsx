bc99e186be13db2e2f5cbc6960814176bf3d91a6
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-03-13 02:24:49 +0600

    added new js file

--- my-es6-projects/src/components/Sunglass/Sunglass.jsx
+++ my-es6-projects/src/components/Sunglass/Sunglass.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import './Sunglass.css';
 import Watch from '../Watch/Watch';
+import add from '../../Utils/calculate';
 const Sunglass = () => {
+    const first = 55;
+    const second = 20;
+    const sum = add(first, second);
     return (
         <div>
             <Watch>
