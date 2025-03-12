1662e29e72f4292c2b8769c6fdcdf6ed2bdf55d1
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-03-13 02:11:48 +0600

    update the code

--- my-es6-projects/src/components/Sunglass/Sunglass.jsx
+++ my-es6-projects/src/components/Sunglass/Sunglass.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import './Sunglass.css';
+import Watch from '../Watch/Watch';
 const Sunglass = () => {
     return (
         <div>
+            <Watch>
 
+            </Watch>
         </div>
     );
 };
