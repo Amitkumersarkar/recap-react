b22bcdb81f701af6b04cd5787600cda32bae408f
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-04-15 21:05:30 +0600

    update file

--- recap-react/src/Friends.jsx
+++ recap-react/src/Friends.jsx
@@ -1,8 +1,21 @@
-export default function Friends() {
+import { useEffect, useState } from "react"
+import Friend from "./friend";
 
+export default function Friends() {
+    const [friends, setFriend] = useState([]);
+
+    //to load data by declared dependency with callback function
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(data => setFriend(data))
+    }, [])
     return (
         <div>
-            <h2>Hello</h2>
+            <h2>Friends : {friends.length}</h2>
+            {
+                friends.map(friend => <Friend friend={friend}></Friend>)
+            }
         </div>
     )
 }
