976461039f01ed91a2f8b563d8c023f039089454
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-04-16 22:47:56 +0600

    update the code

--- recap-react/src/Friend.jsx
+++ recap-react/src/Friend.jsx
@@ -1,11 +1,12 @@
 // import React from 'react';
 
 export default function Friend({ friend }) {
-    const { name, email } = friend;
+    const { name, email, address } = friend;
     return (
         <div>
             <h3>Name : {name}</h3>
             <p>Email : {email} </p>
+            <p>Address : {address}</p>
         </div>
     )
 }
